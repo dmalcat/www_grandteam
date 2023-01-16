b0610f14004930f64dfd5abd507931d5fb5bec6d
davidmalcat <david>
2023-01-16 11:33:04 +0100

    index moved to root, images path fix

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (file_exists(__DIR__ . '/.maintenance.php')) {
 	require __DIR__ . '/.maintenance.php';
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 FreshBangApp\Bootstrap::boot()
 	->createContainer()
